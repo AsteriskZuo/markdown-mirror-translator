b5f7b0ac59277f99fcd5b00e6dc4e86a4aa0b778
asteriskzuo <zuoyuhsywn@hotmail.com>
2026-05-14 14:51:13 +0800

    feat: wire translator shell activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { registerSaveTranslatedFileCommand } from './commands/saveTranslatedFile';
+import { registerTranslateCurrentFileCommand } from './commands/translateCurrentFile';
+import { translatedDocumentScheme, TranslatedDocumentProvider } from './document/translatedDocumentProvider';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
+	const translatedDocumentProvider = new TranslatedDocumentProvider();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdown-mirror-translator" is now active!');
+	context.subscriptions.push(
+		vscode.workspace.registerTextDocumentContentProvider(translatedDocumentScheme, translatedDocumentProvider),
+		translatedDocumentProvider,
+	);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('markdown-mirror-translator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Markdown Mirror Translator!');
-	});
-
-	context.subscriptions.push(disposable);
+	registerTranslateCurrentFileCommand(context, translatedDocumentProvider);
+	registerSaveTranslatedFileCommand(context, translatedDocumentProvider);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void {}
